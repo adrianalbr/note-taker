929f92db6621b7441e997e31afdd1e75d2e273ab
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2020-11-15 11:59:02 -0500

    added get routes to server.js

--- Develop/serve.js
+++ Develop/serve.js
@@ -1,6 +1,10 @@
 
-//Require express
+//Require express, path, and fs
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+//require array of notes from somewhere...db.json...
 
 //Create instance of express
 const app = express();
@@ -11,3 +15,38 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Use public root folder
+app.use(express.static(__dirname + '/public'));
+
+//Listen
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+
+  //Routes
+
+  //Get routes
+
+  app.get("/", function (req, res){
+      res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+  app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res){
+    return res.json(JSON.parse(fs.readFileSync(".db/db.json")));
+});
+
+//Post routes
+
+app.post("/api/notes", function (req, res){
+    let newNoteRequest = req.body;
+    console.log("New request: ", newNoteRequest);
+
+    
+
+})
+
